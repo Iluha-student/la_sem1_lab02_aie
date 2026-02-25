4a770d577fa2d63d2551dbb356bd22d3d8d650c0
Илья Кузьмичев <ilya@MacBook-Air-Ila-3.local>
2026-02-25 13:51:38 +0300

    Homework solution

--- CSR.py
+++ CSR.py
@@ -120,8 +120,10 @@ class CSRMatrix(Matrix):
         from CSC import CSCMatrix
 
         m, n = self.shape
-        transposed_data = []
-        transposed_indices = []
+        total_nnz = len(self.data)
+
+        transposed_data = [0.0] * total_nnz
+        transposed_indices = [0] * total_nnz
         transposed_indptr = [0] * (n + 1)
 
         # Подсчет ненулевых элементов в каждом столбце
@@ -141,8 +143,8 @@ class CSRMatrix(Matrix):
             for p in range(self.indptr[i], self.indptr[i + 1]):
                 j = self.indices[p]
                 pos = next_pos[j]
-                transposed_data.append(self.data[p])
-                transposed_indices.append(i)
+                transposed_data[pos] = self.data[p]
+                transposed_indices[pos] = i
                 next_pos[j] += 1
 
         return CSCMatrix(transposed_data, transposed_indices, transposed_indptr, (n, m))
